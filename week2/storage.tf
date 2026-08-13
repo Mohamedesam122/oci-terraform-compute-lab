dae0c8d3c06422e4dfc5f6f151832768eb071fb7
Mohamedesam122 <162317445+Mohamedesam122@users.noreply.github.com>
2026-08-13 05:28:56 +0300

    Remove comments from storage.tf
    Removed comments explaining the mount target and export set.

--- week2/storage.tf
+++ week2/storage.tf
@@ -13,8 +13,7 @@ resource "oci_file_storage_mount_target" "mt" {
   freeform_tags        = local.freeform_tags
 }
 
-# The mount target automatically provisions its own export set;
-# we just attach an export for our file system to it.
+
 resource "oci_file_storage_export" "export" {
   export_set_id  = oci_file_storage_mount_target.mt.export_set_id
   file_system_id = oci_file_storage_file_system.fs.id
